a10cd9012e991de93740a3395ed9078f2d51672a
Christian_Windows_Desktop <chgeovany.15@gmail.com>
2022-06-06 20:49:49 -0500

    mejorado binary search

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -20,22 +20,25 @@ print(binary_search(my_list,3))
 print(binary_search(my_list,-1))
 '''
 
+from ast import Return
+
+
 def binary_search(list,item,low,high):
     if low>high:
-        print("Not found")
+        return None
     else:
         mid = (low+high)//2
         guess = list[mid]
         if guess == item:
-            print(f"Founded in index {mid}")
+            return mid
         elif guess > item:
-            binary_search(list,item,low,mid-1)
+            return binary_search(list,item,low,mid-1)
         else:
-            binary_search(list,item,mid+1,high)
+            return binary_search(list,item,mid+1,high)
 
 
 my_list = [1,3,5,7,9,11,13,15,17]
 
-binary_search(my_list,11,0,len(my_list)-1)
-binary_search(my_list,-1,0,len(my_list)-1)
+print(binary_search(my_list,11,0,len(my_list)-1))
+print(binary_search(my_list,-1,0,len(my_list)-1))
     
